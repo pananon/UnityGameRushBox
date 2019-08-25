b21e7a2cfc7cd7b192e4312dbf466dd571229b1a
pananon <pandeyjss01@gmail.com>
2019-08-26 00:35:13 +0530

    Final commit

---
+++ Assets/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Unity.UI;
+public class Score : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public Rigidbody rb;
+    public float FarwardForce = 1500f;
+    public float SideForce = 500f;
+    public float Accel = 1000f;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0, 0, FarwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(SideForce * Time.deltaTime , 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, Accel * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -Accel * Time.deltaTime);
+        }
+    }
+}
